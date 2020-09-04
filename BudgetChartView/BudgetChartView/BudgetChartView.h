b2684bd284b592a420060b479cf0ceafd2018fc6
Ali Hasanoğlu <hasanogliali80@gmail.com>
2020-09-04 11:29:19 +0300

    BudgetChartView created.

---
+++ BudgetChartView/BudgetChartView/BudgetChartView.h
@@ -0,0 +1,19 @@
+//
+//  BudgetChartView.h
+//  BudgetChartView
+//
+//  Created by Ali Hasanoğlu on 3.09.2020.
+//  Copyright © 2020 Ali Hasanoğlu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BudgetChartView.
+FOUNDATION_EXPORT double BudgetChartViewVersionNumber;
+
+//! Project version string for BudgetChartView.
+FOUNDATION_EXPORT const unsigned char BudgetChartViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BudgetChartView/PublicHeader.h>
+
+
